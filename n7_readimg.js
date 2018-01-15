2410d1805207af0402e70fbf20b1325a1ae47cf7
Chasen-Zhang <787025321@qq.com>
2018-01-15 10:54:18 +0800

    test9009

--- n7_readimg.js
+++ n7_readimg.js
@@ -14,7 +14,7 @@ http.createServer(function  (request,  response)  {
 		console.log(4);
 		console.log(89);
 		console.log('test2');
-		//Signed-off
+		//Signed-offooo
     }
 }).listen(8000);
 console.log('Server  running  at  http://127.0.0.1:8000/');
